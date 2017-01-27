96388f4460cfffd63f125b1bbfdf8eada4291691
Amil Khare <amilaxelius@gmail.com>
2017-01-27 09:23:01 +0530

    Update solution.py

--- Project1/solution.py
+++ Project1/solution.py
@@ -16,7 +16,7 @@ unitlist = row_units + column_units + square_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
-def assign_value(values, box, value):
+def assign_value(values, box, value):   #This function is used to change the Value of the sudoku( stored as Dictionary)
     """
     Please use this function to update your values dictionary!
     Assigns a value to a given box. If it updates the board record it.
@@ -26,7 +26,7 @@ def assign_value(values, box, value):
         assignments.append(values.copy())
     return values
 
-def naked_twins(values):
+def naked_twins(values):    #This function is used to implement the naked twins stratergy
     """Eliminate values using the naked twins strategy.
     Args:
         values(dict): a dictionary of the form {'box_name': '123456789', ...}
@@ -37,21 +37,22 @@ def naked_twins(values):
 
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
-    twins1 = []
-    twins2 = []
-    size_2 = [i for i in values.keys() if len(values[i])==2]
+    twins1 = []     # Stores the first part of pair of boxes having only 2 totally same values
+    twins2 = []     # Stores the second part of pair of boxes having only 2 totally same values
+
+    size_2 = [i for i in values.keys() if len(values[i])==2]    #stores all boxes of sudoku with length 2( having 2 values)
     for i in size_2:
-        for peer in peers[i]:
+        for peer in peers[i]:      #For every box with length 2 , iterate over all it's peers
             if values[peer]==values[i]:
-                twins1.append(i)
-                twins2.append(peer)
-    for first_twin,second_twin in zip(twins1,twins2):
+                twins1.append(i)    #If any one of the peer has same numbers as the chose pair's part and length 2 then
+                twins2.append(peer) # append the first part to twins1 , and other to twins2
+    for first_twin,second_twin in zip(twins1,twins2):   #Iterating over the twins/pairs
         for unit in unitlist:
             if first_twin in unit and second_twin in unit:
                 for box in unit:
-                    if box!=first_twin and box!=second_twin:
+                    if box!=first_twin and box!=second_twin:    #Checking in which unit both our pairs lie
                         for digit in values[first_twin]:
-                            values[box] = values[box].replace(digit,'')
+                            values[box] = values[box].replace(digit,'') #Removing the 2 numbers/digits in our pairs from the entire unit
 
     return values
 
@@ -70,7 +71,7 @@ def grid_values(grid):
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
     # pass
-    sudoku = []
+    sudoku = []     #Stores the Sudoku in form of a dictionary
     digits = '123456789'
     for c in grid:
         if c in digits:
@@ -88,7 +89,7 @@ def display(values):
         values(dict): The sudoku in dictionary form
     """
     # pass
-    width = 1+max(len(values[s]) for s in boxes)
+    width = 1+max(len(values[s]) for s in boxes)   
     line = '+'.join(['-'*(width*3)]*3)
     for r in rows:
         print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
@@ -98,6 +99,11 @@ def display(values):
 
 def eliminate(values):
     # pass
+    """
+    Go through all the boxes, and whenever there is a box with a value, eliminate this value from the values of all its peers.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
     single_constraint = [i for i in values.keys() if len(values[i])==1]
     for i in single_constraint:
         val = values[i]
@@ -108,6 +114,11 @@ def eliminate(values):
 
 def only_choice(values):
     # pass
+    """
+    Go through all the units, and whenever there is a unit with a value that only fits in one box, assign the value to this box.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
     for unit in unitlist:
         for digit in '123456789':
             possible_digit_places = []
@@ -121,7 +132,16 @@ def only_choice(values):
 
 def reduce_puzzle(values):
     # pass
+    """
+    Iterate eliminate() and only_choice(). If at some point, there is a box with no available values, return False.
+    If the sudoku is solved, return the sudoku.
+    If after an iteration of both functions, the sudoku remains the same, return the sudoku.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
     stop = False
+    # Recursive function to keep on implementing the strategies that were discussed above until a solution is reached OR 
+    # there exists no solution or the sudoku cannot be further solved
     while not stop:
         
         solved_uptil_now = 0
@@ -137,7 +157,12 @@ def reduce_puzzle(values):
             stop = True      
     return values
 
-def search(values):
+def search(values):     
+    # "Using depth-first search and propagation, created a search tree and solved the sudoku."
+    # Firstly we reduce the puzzle using our strategies
+    # Choosing the box with fewest possibilities we expand the tree and go over each of it's possible digits
+    # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+    # else explore other possibilites. If no other possibilities are there then return FALSE
     # pass
     values = reduce_puzzle(values)
     if values == False:
@@ -171,12 +196,12 @@ def solve(grid):
     Returns:
         The string representing the final sudoku grid. False if no solution exists.
     """
-    str_to_values = grid_values(grid)
-    global unitlist
-    unitlist = unitlist+diag2+diag1
-    ans = search(str_to_values)
+    str_to_values = grid_values(grid) #Convert String to a dictionary for sudoku
+    global unitlist                 
+    unitlist = unitlist+diag2+diag1   #Remove this line if you don't need to solve Diagonal Sudoku ( This is only to add diagonal constraint)
+    ans = search(str_to_values)     #Call the solving function
     if ans == False:
-        return False
+        return False            #If false No solution exists
     return ans
     # values_to_str = ""
     # for i in values.keys()
